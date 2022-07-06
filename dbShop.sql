fdf735951ad935ea1a66030a366438b5d4246074
anhchienlyka <phamchien96nd@gmail.com>
2022-07-06 09:34:06 +0700

    'op'

---
+++ dbShop.sql
@@ -0,0 +1,291 @@
+﻿CREATE DATABASE [Shop]
+
+---tài khoản
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Accounts](
+	[AccountID] [int] IDENTITY(1,1) NOT NULL,
+	[Phone] [varchar](12) NULL,
+	[Email] [nvarchar](50) NULL,
+	[Password] [nvarchar](50) NULL,
+	[Salt] [nchar](100) NULL,
+	---Salt chuỗi mã hóa mật khẩu
+	[Active] [bit] NOT NULL,
+	---trạng thái tài khoản
+	[FullName] [nvarchar](150) NULL,
+	[RoleID] [int] NULL,
+	---Role quyền truy cập Admin hay nv
+	[LastLogin] [datetime] NULL,
+	---lần đăng nhập cuối
+	[CreateDate] [datetime] NULL,
+	---ngày đăng kí tài khoản
+ CONSTRAINT [PK_Accounts] PRIMARY KEY CLUSTERED 
+(
+	[AccountID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+--danh mục
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CatID] [int] IDENTITY(1,1) NOT NULL,
+	[CatName] [nvarchar](250) NULL,
+	[Description] [nvarchar](max) NULL,
+	---mô tả
+	[ParentID] [int] NULL,
+	---kiểu phân cấp VD: danh mục cha,con
+	[Levels] [int] NULL,
+	[Ordering] [int] NULL,
+	---sắp xếp
+	[Published] [bit] NOT NULL,
+	---danh mục còn bán hay không
+	[Thumb] [nvarchar](250) NULL,
+	---ảnh đại diện cho danh mục
+	[Title] [nvarchar](250) NULL,
+	[Alias] [nvarchar](250) NULL,
+	---thay thế tên danh mục
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[CatID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+---quản lí khách hàng
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerID] [int] IDENTITY(1,1) NOT NULL,
+	[FullName] [nvarchar](255) NULL,
+	[Address] [nvarchar](255) NULL,
+	[Email] [nchar](150) NULL,
+	[Phone] [varchar](12) NULL,
+	[LocationID] [int] NULL,
+	---vị trí khách hàng số nhà tên đường
+	[CreateDate] [datetime] NULL,
+	---ngày đăng kí tk
+	[Password] [nvarchar](50) NULL,
+	[Salt] [nchar](100) NULL,
+	---chuỗi mã hóa mật khẩu
+	[LastLogin] [datetime] NULL,
+	---lần đăng  nhập cuối
+	[Active] [bit] NOT NULL,
+	---trạng thái tài khoản
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[CustomerID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerID] [int] NULL,
+	---ID quản lí đơn hàng
+	[OrderDate] [datetime] NULL,
+	---ngày mua
+	[ShipDate] [datetime] NULL,
+	[DeliveryDate] [datetime] NULL,
+	---ngày giao hàng
+	[TransactStatusID] [int] NOT NULL,
+	---trạng thái đơn hàng
+	[Deleted] [bit] NOT NULL,
+	---chuyển trạng thái đơn hàng sang hủy
+	[Paid] [bit] NOT NULL,
+	---kiểm tra thanh toán
+	[PaymentDate] [datetime] NULL,
+	---ngày thanh toán
+	[TotalMoney] [int] NOT NULL,
+	---tổng tiền
+	[PaymentID] [int] NULL,
+	---hình thức thanh toán
+	[Note] [nvarchar](max) NULL,
+	---ghi chú đơn hàng
+	[Address] [nvarchar](max) NULL,
+	[LocationID] [int] NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+---chi tiết đơn hàng
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OrderDetails](
+	[OrderDetailID] [int] IDENTITY(1,1) NOT NULL,
+	[OrderID] [int] NULL,
+	[ProductID] [int] NULL,
+	[OrderNumber] [int] NULL,
+	[Amount] [int] NULL,
+	[Discount] [int] NULL,
+	[TotalMoney] [int] NULL,
+	[CreateDate] [datetime] NULL,
+	[Price] [int] NULL,
+ CONSTRAINT [PK_OrderDetails] PRIMARY KEY CLUSTERED 
+(
+	[OrderDetailID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+---quản lí sản phẩm
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Products](
+	[ProductID] [int] IDENTITY(1,1) NOT NULL,
+	[ProductName] [nvarchar](255) NOT NULL,
+	[Description] [nvarchar](max) NULL,
+	---mô tả dài sp
+	[CatID] [int] NULL,
+	---sp thuộc danh mục nào
+	
+	[PurchassePrice] [int] NULL,
+	[Price] [int] NULL,
+	---giá sản phẩm
+	[Discount] [int] NULL,
+	[Thumb] [nvarchar](255) NULL,
+	---ảnh sp
+	[DateCreated] [datetime] NULL,
+	---ngày tạo sp
+	[DateModified] [datetime] NULL,
+	---ngày chỉnh sửa sp
+	[BestSellers] [bit] NOT NULL,
+	---sản phẩm mua nhiều
+	[HomeFlag] [bit] NOT NULL,
+	---ghim sp lên trang chủ
+	[Active] [bit] NOT NULL,
+	---sp còn bán hay không
+	[Tags] [nvarchar](max) NULL,
+	[Title] [nvarchar](255) NULL,
+	[Alias] [nvarchar](255) NULL,
+	[UnitsInStock] [int] NULL,
+	---sp trong kho còn bao nhiêu
+	[SupplierId] [int] NULL,
+ CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+---quản lí quyền truy cập
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Roles](
+	[RoleID] [int] IDENTITY(1,1) NOT NULL,
+	[RoleName] [nvarchar](50) NULL,
+	[Description] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED 
+(
+	[RoleID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+---trạng thái đơn hàng
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TransactStatus](
+	[TransactStatusID] [int] IDENTITY(1,1) NOT NULL,
+	[Status] [nvarchar](50) NULL,
+	---trạng thái đơn hàng
+	[Description] [nvarchar](max) NULL,
+ CONSTRAINT [PK_TransactStatus] PRIMARY KEY CLUSTERED 
+(
+	[TransactStatusID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+
+---quản lý nhà cung cấp
+GO
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Supplier](
+	[SupplierId] [int] IDENTITY(1,1) NOT NULL,
+	---mã nhà cung cấp
+	[Name] [nvarchar](50) NULL,
+	[Email] [nvarchar](50) NULL,
+	[Phone] [nvarchar](50) NULL,
+		[Address] [nvarchar](255) NULL,
+	CONSTRAINT [PK_Supplier] PRIMARY KEY CLUSTERED 
+(
+	[SupplierId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] 
+
+GO
+ALTER TABLE [dbo].[Accounts]  WITH CHECK ADD  CONSTRAINT [FK_Accounts_Roles] FOREIGN KEY([RoleID])
+REFERENCES [dbo].[Roles] ([RoleID])
+GO
+ALTER TABLE [dbo].[Accounts] CHECK CONSTRAINT [FK_Accounts_Roles]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Orders] FOREIGN KEY([OrderID])
+REFERENCES [dbo].[Orders] ([OrderID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Orders]
+GO
+ALTER TABLE [dbo].[OrderDetails]  WITH CHECK ADD  CONSTRAINT [FK_OrderDetails_Products] FOREIGN KEY([ProductID])
+REFERENCES [dbo].[Products] ([ProductID])
+GO
+ALTER TABLE [dbo].[OrderDetails] CHECK CONSTRAINT [FK_OrderDetails_Products]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerID])
+REFERENCES [dbo].[Customers] ([CustomerID])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_TransactStatus] FOREIGN KEY([TransactStatusID])
+REFERENCES [dbo].[TransactStatus] ([TransactStatusID])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_TransactStatus]
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Categories] FOREIGN KEY([CatID])
+REFERENCES [dbo].[Categories] ([CatID])
+GO
+ALTER TABLE [dbo].[Products]  WITH CHECK ADD  CONSTRAINT [FK_Products_Supplier] FOREIGN KEY([SupplierId])
+REFERENCES [dbo].[Supplier] ([SupplierId])
+GO
+ALTER TABLE [dbo].[Products] CHECK CONSTRAINT [FK_Products_Categories]
+
+
+GO
+SET IDENTITY_INSERT [dbo].[Roles] ON 
+INSERT [dbo].[Roles] ([RoleID],[RoleName], [Description]) VALUES ( 2,N'Admin', N'Admin')
+INSERT [dbo].[Roles] ([RoleID],[RoleName], [Description]) VALUES ( 3,N'Staff', N'Nhân viên')
+SET IDENTITY_INSERT [dbo].[Roles] OFF
+
+
